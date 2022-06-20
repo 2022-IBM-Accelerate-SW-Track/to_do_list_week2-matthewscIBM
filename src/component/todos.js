10f4af3a03feb78c90505ebee7f37ffba610ef47
matthewscIBM <107156717+matthewscIBM@users.noreply.github.com>
2022-06-20 16:40:23 -0400

    Update todos.js

--- src/component/todos.js
+++ src/component/todos.js
@@ -10,7 +10,7 @@ import ListItemText
 // 3. The map function is called to assign each array item with a key
 // 4. Think of lines 14-23 as a loop. For each todo in the todo list, we want to give the list item
 // a key, and it's own card shown in the UI
-const Todos = ({ todos }) => {
+const Todos = ({ todos, deleteTodo }) => {
   const todoList = todos.length ? (
     todos.map((todo) => {
       return (
@@ -19,9 +19,9 @@ const Todos = ({ todos }) => {
             {/* Remember, we set the local state of this todo item when the user submits the form in 
             AddTodo.js. All we need to do is return the todo list item {todo.content} */}
                <ListItemButton component="a" href="#simple-list"> 
-                 const checkBox = { 
-                   paddingLeft: 0, 
-                   color: "primary", 
+                 <const checkBox = { 
+                   style = paddingLeft: 0, 
+                   color = "primary", 
                  } 
      <ListItemText primary={todo.content}/> secondary 
    </ListItemButton> 
